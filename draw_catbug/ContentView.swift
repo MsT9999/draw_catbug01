b982fe8f9ef041d2833e0c869d0356781aa0881c
MsT9999 <00657027@email.ntou.edu.tw>
2020-10-02 02:58:37 -0700

    20201002  The first test of source control if it works.

---
+++ draw_catbug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  draw_catbug
+//
+//  Created by C9 on 2020/10/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("this is Catbug!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ draw_catbug/draw_catbugApp.swift
@@ -0,0 +1,17 @@
+//
+//  draw_catbugApp.swift
+//  draw_catbug
+//
+//  Created by C9 on 2020/10/2.
+//
+
+import SwiftUI
+
+@main
+struct draw_catbugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
